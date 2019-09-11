7c732bc632517a733bac4e52686eef0b8c6e21b4
Alberto Cuoci <alberto.cuoci@gmail.com>
2019-09-11 13:21:58 +0200

    Update advection_diffusion_1d.m

--- codes/advection_diffusion_1d.m
+++ codes/advection_diffusion_1d.m
@@ -17,7 +17,7 @@
 %                                                                         %
 %	License                                                               %
 %                                                                         %
-%   Copyright(C) 2017 Alberto Cuoci                                       %
+%   Copyright(C) 2019 Alberto Cuoci                                       %
 %   Matlab4CFDofRF is free software: you can redistribute it and/or       %
 %   modify it under the terms of the GNU General Public License as        %
 %   published by the Free Software Foundation, either version 3 of the    %
@@ -41,90 +41,99 @@
 close all;
 clear variables;
 
-% Data
+% User-defined data
 %-------------------------------------------------------------------------%
 np=21;              % number of grid points                
 nstep=100;          % number of time steps
-length=2.0;         % domain length [m]
-h=length/(np-1);    % grid step [m]
+L=2.0;              % domain length [m]
 dt=0.05;            % time step [s]
 u=1;                % velocity [m/s]
 D=0.05;             % diffusion coefficient [m2/s]
 A=0.5;              % amplitude of initial solution
 k=1;                % wave number [1/m]
 
-% Memory allocation
+% Pre-processing of user-defined data
 %-------------------------------------------------------------------------%
-f=zeros(np,1);      % temporary numerical solution
-y=zeros(np,1);      % current numerical solution
+% Grid step calculation
+h=L/(np-1);         % grid step [m]
+
+% Memory allocation
+fo=zeros(np,1);     % temporary numerical solution
+f=zeros(np,1);      % current numerical solution
 a=zeros(np,1);      % exact solution
+
+% Initial solution
+for i=1:np
+	f(i)=A*sin(2*pi*k*h*(i-1)); 
+end
+
+% Check the stability conditions on time step
+Co = u*dt/h;                        % Courant number
+Di = D*dt/h^2;                      % Diffusion number
+dt_max = min(1*h/u, 0.5*h*h/D);     % Maximum allowed time step
+fprintf('Co=%f, Di=%f, dt=%f, dt(max)=%f\n', Co, Di, dt, dt_max);
 
 % Video setup
 %-------------------------------------------------------------------------%
-v = VideoWriter('advection_diffusion_1d.mp4', 'MPEG-4');
-open(v);
+video_name = 'advection_diffusion_1d.mp4';
+videompg4 = VideoWriter(video_name, 'MPEG-4');
+open(videompg4);
 
-% Initial solution
+% Advancing in time
 %-------------------------------------------------------------------------%
-for i=1:np
-	y(i)=A*sin(2*pi*k*h*(i-1)); 
-end
-
-% Loop over time
-%-------------------------------------------------------------------------%
-t=0.; 
-for m=1:nstep 
-	
-    % Exact solution
+t = 0.;
+for m=1:nstep
+    
+    % Update the analytical solution
     for i=1:np 
 		a(i) = A*exp(-4*pi*pi*k*k*D*t)*sin(2*pi*k*(h*(i-1)-u*t)); 
-    end
+    end  
     
-    % Integrals (post processing only)
+    % Squared areas below the analytical and numerical solutions
     a2_int = 0.;
-    y2_int = 0.;
+    f2_int = 0.;
     for i=1:np-1
          a2_int = a2_int + h/2*(a(i)^2+a(i+1)^2);
-         y2_int = y2_int + h/2*(y(i)^2+y(i+1)^2);
-    end
-         
-    % Plots
-    message = sprintf('time=%d\na^2(int)=%d\ny^2(int)=%d', t, a2_int, y2_int);
-	hold off; plot([0:h:length],y,'linewidth',2); axis([0 length -1, 1]); % plot num. 
-	hold on; plot([0:h:length],a,'r','linewidth',2);                      % plot exact
-    hold on; legend('numerical', 'exact');                  % legend
+         f2_int = f2_int + h/2*(f(i)^2+f(i+1)^2);
+    end  
+    
+    % Graphical output
+    message = sprintf('time=%d\na^2(int)=%d\ny^2(int)=%d', t, a2_int, f2_int);
+	hold off; plot(0:h:L,f,'linewidth',2); axis([0 L -1, 1]); % plot num. 
+	hold on; plot(0:h:L,a,'r','linewidth',2);                 % plot exact
+    hold on; legend('numerical', 'exact');                    % legend
     xlabel('spatial coordinate [m]');
     ylabel('solution');    
     time = annotation('textbox',[0.15 0.8 0.1 0.1],'String',message,'EdgeColor','none');
     frame = getframe(gcf);
-    writeVideo(v,frame);
+    writeVideo(videompg4,frame);
     delete(time);
     
-    % Temporary solution 
-	f=y; 
-    
-    % Numerical solution (internal points)
+    % Forward Euler method
+    fo=f;   
     for i=2:np-1 
-		y(i) = f(i)-(u*dt/2/h)*(f(i+1)-f(i-1))+...    % advection
-			   D*(dt/h^2)*(f(i+1)-2*f(i)+f(i-1));     % diffusion
+		f(i) = fo(i)-(u*dt/2/h)*(fo(i+1)-fo(i-1))+...      % advection
+			   D*(dt/h^2)*(fo(i+1)-2*fo(i)+fo(i-1));   % diffusion
     end 
     
-    % Numerical solution (periodic boundary conditions)
-	y(np) = f(np)-(u*dt/2/h)*(f(2)-f(np-1))+...
-            D*(dt/h^2)*(f(2)-2*f(np)+f(np-1)); 
-	y(1)  = y(np);
+    % Periodic boundary condition
+    f(np) = fo(np)-(u*dt/2/h)*(fo(2)-fo(np-1))+...
+            D*(dt/h^2)*(fo(2)-2*fo(np)+fo(np-1)); 
+    f(1)  = f(np);
     
-    % Calculate error
+    % Update the error between numerical and analytical solution
     E = 0;
     for i=1:np 
-        E = E + (y(i)-a(i))^2;
+        E = E + (f(i)-a(i))^2;
     end
-    E = h*sqrt(E);
+    E = h*sqrt(E); 
     
-    % Advance time
-	t=t+dt; 
-    fprintf('time=%d E=%e\n', t, E);
+    % New time step
+    t=t+dt; 
     
+    % Print the current time (every 25 steps)
+    if (mod(m,25)==1), fprintf('time=%d E=%e\n', t, E); end
 end
 
-close(v);
+% Closing the video stream
+close(videompg4);
